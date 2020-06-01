694511868be54b69cb81087c99c1f13f7bb21dc1
masoda1112 <wendy.odakura@gmail.com>
2020-06-01 10:44:14 +0900

    現在地取得確認用のlocationclass追加

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Map from './map';
 import Info from './info';
 import Button from './button'
+//import NodeEnvironment from 'jest-environment-node';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {lat:0,lon:0};
+  }
   render()
   {
+    function Location(){
+      navigator.geolocation.getCurrentPosition(function(position){
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.setState({ lat : this.latitude,lon : this.longitude}); 
+      });}
     return (
     <div>
       <h1 class = 'title'>近くで人気のお店です</h1>
@@ -16,7 +27,11 @@ class App extends React.Component{
        <Map/>
       </div>
       <div class = 'button'>
-       <Button/>
+       <Button　onClick={()=>{Location()}}></Button>
+      </div>
+      <div class = 'location'>
+       <p>{this.state.lat}</p>
+       <p>{this.state.lon}</p>
       </div>
       
     </div>
